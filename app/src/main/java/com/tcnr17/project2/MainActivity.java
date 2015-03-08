4bce87e2c60efffd811cb410571fd8045bb85b76
Maggie <mamamag77@gmail.com>
2015-03-08 20:23:09 +0800

    add another 2 fragments

--- app/src/main/java/com/tcnr17/project2/MainActivity.java
+++ app/src/main/java/com/tcnr17/project2/MainActivity.java
@@ -58,7 +58,18 @@ public class MainActivity extends ActionBarActivity
                         .replace(R.id.container, new Act0Fragment())
                         .commit();
                 break;
+            case 1:
 
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new Act1Fragment())
+                        .commit();
+                break;
+            case 2:
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new Act2Fragment())
+                        .commit();
+                break;
             default:
 
                 fragmentManager.beginTransaction()
@@ -183,6 +194,78 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_act0, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+//            ((MainActivity) activity).onSectionAttached(
+//                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+    public static class Act1Fragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Act1Fragment newInstance(int sectionNumber) {
+            Act1Fragment fragment = new Act1Fragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public Act1Fragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_act1, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+//            ((MainActivity) activity).onSectionAttached(
+//                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+    public static class Act2Fragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Act1Fragment newInstance(int sectionNumber) {
+            Act1Fragment fragment = new Act1Fragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public Act2Fragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_act2, container, false);
             return rootView;
         }
 
